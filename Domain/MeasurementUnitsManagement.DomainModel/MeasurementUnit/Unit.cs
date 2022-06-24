36916c05321e2b7486b0b8579ca2e8da32c75ea0
fahimeh <fahimeh.barzegar@gmail.com>
2022-06-24 10:54:36 +0430

    Comlete Absract Unit

--- Domain/MeasurementUnitsManagement.DomainModel/MeasurementUnit/Unit.cs
+++ Domain/MeasurementUnitsManagement.DomainModel/MeasurementUnit/Unit.cs
@@ -1,6 +1,14 @@
-﻿namespace MeasurementUnitsManagement.DomainModel.MeasurementUnit
+namespace MeasurementUnitsManagement.DomainModel.MeasurementUnit
 {
 	public class Unit
 	{
+		public virtual void ConvertToBaseUnit(decimal value, int factorUnitId, int baseUnitId)
+		{
+
+		}
+		public virtual void ConvertFromBaseUnit(decimal value, int baseUnitId, int factorUnitId)
+		{
+
+		}
 	}
 }
